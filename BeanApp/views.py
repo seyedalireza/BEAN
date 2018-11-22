41201c0c84ba8cd15352d8756064b2d1a566e5b9
seyedalireza <shosseini@ce.sharif.edu>
2018-11-22 13:04:41 +0330

    sign up compelted

--- BeanApp/views.py
+++ BeanApp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -6,7 +7,7 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 
-from BeanApp.forms import SignUpForm
+from BeanApp.forms import SignUpForm, ContactForm
 
 
 def signup(request):
@@ -52,3 +53,20 @@ def login(request):
     return render(request, "login_form.html", {
         "error": error
     })
+
+
+def contact_view(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['‫‪ostaduj@fastmail.com‬‬'])
+            except Exception as exec:
+                print("invalid ")
+            return redirect('success')#change text
+    return render(request, "email.html", {'form': form})
