d1c54323a056c0e014a16479b7d0e535d96cde6e
seyedalireza <shosseini@ce.sharif.edu>
2018-11-22 14:22:19 +0330

    d

--- BeanApp/forms.py
+++ BeanApp/forms.py
@@ -1,13 +1,17 @@
 from django import forms
+from django.conf.locale import id
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField( max_length=30, required=False, help_text='Optional' ,widget=forms.TextInput(attrs = {
+        "" : "id_first_name" , "name" : "نام"}))
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.',widget=forms.TextInput(attrs = {
+        "id" : "id_first_name" , "name" : "نام"}))
+    email = forms.EmailField(max_length=254 ,  id = "id_email", help_text='Required. Inform a valid email address.' , widget=forms.TextInput(attrs = {
+        "id" : "id_first_name" , "name" : "نام"}))
 
     class Meta:
         model = User
