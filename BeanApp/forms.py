dd983688fd1583eb3d5431831dfedb1fc9527cd8
seyedalireza <shosseini@ce.sharif.edu>
2018-11-23 13:39:23 +0330

    debug

--- BeanApp/forms.py
+++ BeanApp/forms.py
@@ -13,6 +13,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    type = forms.ChoiceField(choices=GROUP_CHOICES, widget=forms.RadioSelect())
 
     class Meta:
         model = User
